b3ba2866872a101fef7100ca6935aef2a5eed1e5
jonny <55215300+jonathanzxu@users.noreply.github.com>
2023-04-06 10:02:25 -0700

    Update WindowNotInstalled.tsx

--- components/WindowNotInstalled.tsx
+++ components/WindowNotInstalled.tsx
@@ -5,7 +5,11 @@ function WindowNotInstalled() {
           <div className="text-center text-xl text-white font-light">
               Oops! We can&#39;t detect Window.AI in your browser.
           </div>
-          <button className="flex flex-row bg-transparent px-4 py-2 mt-4 rounded-xl border-[1px] border-slate-100 border-opacity-60 hover:border-opacity-100 text-lg text-white font-thin transition-all">
+          <button onClick={
+            () => {
+                window.open('https://windowai.io/', '_blank')
+            }
+          } className="flex flex-row bg-transparent px-4 py-2 mt-4 rounded-xl border-[1px] border-slate-100 border-opacity-60 hover:border-opacity-100 text-lg text-white font-thin transition-all">
               Get Window.AI
           </button>
           <div className="absolute -top-1 right-4 h-4 w-4 rotate-45 bg-gray-900" />
